7c4e6a4337d327f797e51a891acf658d11019c28
DOOMZOOM <104279962+Link-Games@users.noreply.github.com>
2026-02-28 22:23:05 -0800

    Add audio assets, mixers & menu scripts
    Rename misicb to Assets/Audio and add signal audio/video files with metadata; add MainMixer and NewAudioMixer assets. Add MainMenu and SettingsMenu scripts (scene navigation and audio volume control), include recovery scenes, and update MainMenu scene and EditorBuildSettings.

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void playGame ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void quitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
